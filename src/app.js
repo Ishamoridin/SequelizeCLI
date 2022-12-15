a40738d25006426c2e73eedff02f0c6324cd4cd7
Ishamoridin <Ishamoridin@googlemail.com>
2022-12-15 21:13:11 +0000

    finished CRUD

--- src/app.js
+++ src/app.js
@@ -1,28 +1,36 @@
 const yargs=require('yargs');
 const { openSequelizeConnection} = require ('./db/connection');
-const { createMovie, readMovies, updateActor, deleteMovie} = require ('./movie/function');
+const { createMovie, readMovies, updateMovie, deleteMovie, keyChecker} = require ('./movie/function');
 
 async function app(yargsInput) {
     await openSequelizeConnection.sync();
     if (yargsInput.create) {
         // put code to add a movie here
-        await createMovie({title: yargsInput.title, actor: yargsInput.actor, director: yargsInput.director, rating: yargsInput.rating});
+        const newMovie = keyChecker(yargsInput)
+        await createMovie(newMovie);
+        console.log(`Created new movie:`)
+        console.table(newMovie)
     } 
     else if (yargsInput.read) {
         // put code to list movies here
-        let movieFilter = {};
-        if (yargsInput.title){movieFilter.title = yargsInput.title};
-        if (yargsInput.actor){movieFilter.actor = yargsInput.actor};
-        if (yargsInput.director){movieFilter.director = yargsInput.director};
-        if (yargsInput.rating || yargsInput.rating===0){movieFilter.rating = yargsInput.rating};
+        const movieFilter = keyChecker(yargsInput);
         const results = await readMovies({movieFilter});
         console.table(results[0].dataValues)
     } 
     else if (yargsInput.update) {
         //put code to update actor field here
+        const updatedMovie = keyChecker(yargsInput);
+        const results = await updateMovie(updatedMovie, yargsInput.title);
+        console.log(`Updated movie:`)
+        if(results){console.table(updatedMovie)}
     } 
     else if (yargsInput.delete) {
         //put code to delete a movie here
+        const deletedMovie = {title: yargsInput.title};
+        const results = await deleteMovie(deletedMovie);
+        if(results){console.log(`Deleted Movie:`);console.table(deletedMovie)}
+        else {console.log(`No movies with that title.`)}
+
     } 
     else {
         console.log('Unrecognized command');
--- src/movie/function.js
+++ src/movie/function.js
@@ -1,5 +1,17 @@
 const Movie = require('./table');
 
+keyChecker = (input) => {
+    let output = {};
+    if (input.title){output.title = input.title};
+    if (input.actor){output.actor = input.actor};
+    if (input.director){output.director = input.director};
+    if (input.rating || input.rating===0){output.rating = input.rating};
+    return output
+}
+
+exports.keyChecker = keyChecker
+
+
 exports.createMovie = async(movieObj) => {
     try {
         const newMovie = await Movie.create(movieObj);
@@ -18,19 +30,24 @@ exports.readMovies = async(movieFilter) => {
     }
 }
 
-exports.updateMovie = async(movieObj) => {
+exports.updateMovie = async(movieObj, movieFilter) => {
     try {
-        const updatedMovie = await Movie.update(movieObj);
-        console.log(`updated: ${updatedMovie}`)
+        const searchFilter = keyChecker(movieFilter);
+        const updatedMovie = await Movie.update(movieObj, { where: searchFilter}).then((result) => {
+            return result
+        });
+        console.log(`updated: ${updatedMovie}`);
+        return updatedMovie
     } catch (error) {
         console.log(error)
-    }
+    };
+    return results
 }
 
 exports.deleteMovie = async(movieName) => {
     try {
-        const deletedMovie = await Movie.destroy(movieName);
-        console.log(`deleted: ${deletedMovie}`)
+        const deletedMovie = await Movie.destroy({where: movieName}).then((results) => {return results});
+        return deletedMovie
     } catch (error) {
         console.log(error)
     }
